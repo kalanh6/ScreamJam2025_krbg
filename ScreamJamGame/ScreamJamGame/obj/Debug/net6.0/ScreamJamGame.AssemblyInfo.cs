0e11ab7cb077e9487cbf0388020299b64a86a482
GriffinG216 <glg4485@g.rit.edu>
2025-10-16 16:47:02 -0400

    Updated gitignore

--- ScreamJamGame/ScreamJamGame/obj/Debug/net6.0/ScreamJamGame.AssemblyInfo.cs
+++ ScreamJamGame/ScreamJamGame/obj/Debug/net6.0/ScreamJamGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreamJamGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b145d9398a1c5dcdd25d5c72db8c991a0dfe859b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4742ac0a5a605dac3ccd2c81b71602682cc117ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreamJamGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreamJamGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
